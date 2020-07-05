c2ac80fc564601634a1f406ace9a4561778b56cb
Roger Bambinetti <rogerbambinetti@hotmail.com>
2020-07-04 21:28:05 -0300

    database update

---
+++ mobile/src/pages/Challenges.js
@@ -0,0 +1,101 @@
+import React from 'react';
+import { StyleSheet, View, ScrollView, Text, TouchableOpacity } from 'react-native';
+import { FontAwesome5 } from '@expo/vector-icons';
+import { LinearGradient } from 'expo-linear-gradient';
+import { QRCode } from 'react-native-custom-qr-codes-expo';
+
+import ListItem from '../components/ListItem';
+
+export default function Challenges({ navigation }) {
+
+    function handleHomeNavigation() {
+        navigation.navigate('Home');
+    }
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.top}>
+                <View style={styles.challenge}>
+                    <QRCode content='teste' color='#4D4D4D' size={200}/>
+                </View>
+            </View>
+
+            <View style={{ flex: 1 }}>
+                <LinearGradient
+                    style={styles.fader}
+                    colors={['rgba(246, 242, 238, 1)', 'transparent']}
+                    pointerEvents={'none'}
+                />
+                <ScrollView style={styles.bottom}>
+                    <Text style={styles.divider}>Desafios</Text>
+                    <ListItem icon='tag' text='20% de desconto em qualquer cerveja' />
+                    <ListItem icon='tag' text='5% de desconto em  um suco' />
+                </ScrollView>
+            </View>
+            <View style={styles.buttonContainer}>
+                <TouchableOpacity style={styles.button} onPress={handleHomeNavigation}><FontAwesome5 name='arrow-left' size={30} color="#F2F2F2" /></TouchableOpacity>
+            </View>
+        </View>
+    );
+
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#008dd3'
+    },
+    top: {
+        flex: 0,
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 25
+    },
+    bottom: {
+        backgroundColor: '#f6f2ee',
+        flex: 1,
+        borderTopRightRadius: 25,
+        borderTopLeftRadius: 25,
+        paddingHorizontal: 25,
+    },
+    challenge: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 200,
+        height: 200,
+        borderRadius: 10,
+        margin: 25,
+        backgroundColor: 'white'
+    },
+    divider: {
+        fontSize: 15,
+        color: '#CCCCCC',
+        marginVertical: 25,
+        paddingBottom: 10,
+        borderBottomWidth: 0.5,
+        borderBottomColor: '#CCCCCC'
+    },
+    fader: {
+        position: 'absolute',
+        width: '100%',
+        height: 40,
+        zIndex: 5,
+        borderRadius: 25
+    },
+    buttonContainer: {
+        position: 'absolute',
+        bottom: 20,
+        left: 20,
+        zIndex: 5,
+        flexDirection: 'row'
+    },
+    button: {
+        width: 70,
+        height: 70,
+        backgroundColor: '#fcd353',
+        borderRadius: 35,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 10
+    }
+});
